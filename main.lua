25809caaaa64924baf317f499563a64dbc333161
Tom Mason <wheybags@wheybags.com>
2020-01-29 15:06:54 +0100

    template

---
+++ main.lua
@@ -0,0 +1,39 @@
+function love.load()
+end
+
+function love.draw()
+end
+
+function love.resize()
+end
+
+function love.keypressed(key)
+end
+
+
+function love.mousemoved(x,y)
+end
+
+function love.wheelmoved(x,y)
+end
+
+function love.mousepressed(x,y,button)
+
+end
+
+function love.quit()
+
+end
+
+local accumulatedDeltaTime = 0
+function love.update(deltaTime)
+  accumulatedDeltaTime = accumulatedDeltaTime + deltaTime
+
+  local tickTime = 1/60
+
+  while accumulatedDeltaTime > tickTime do
+    --simulation.update(state)
+    accumulatedDeltaTime = accumulatedDeltaTime - tickTime
+  end
+
+end
